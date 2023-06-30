87d6f02667a1141b37365c8b89c8c6db03a6aed1
Ho Phuong <pho295157@gmail.com>
2023-06-30 12:08:59 +0700

    new add

---
+++ main.js
@@ -0,0 +1,11 @@
+// đề bài viết hàm tìm phần tử đầu tiên trong mảng//
+// Viết hàm tại đây
+function getLastElement(array){
+    return array[0];
+}
+// Ví dụ sử dụng
+var animals = ['Monkey', 'Tiger', 'Elephant','a ','b ','c '];
+var result = getLastElement(animals);
+
+console.log(result); // Expected: "Elephant"
+console.log(animals); // Expected: ['Monkey', 'Tiger', 'Elephant']
