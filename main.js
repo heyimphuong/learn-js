f78f947e46bf0c45cb5aded3c4d689f079192b82
Ho Phuong <pho295157@gmail.com>
2023-07-01 17:46:29 +0700

    new

--- main.js
+++ main.js
@@ -21,9 +21,8 @@ var student = {
 student.datebirth = 31122003;// thêm key datebirth
 console.log( student);
 
-
 //tạo Object constructor 
-function Animal (name, leg, speed ){
+function Animal (name, leg, speed){
     this.name = name;
     this.leg = leg;
     this.speed = speed;
@@ -63,3 +62,74 @@ function getNextYear(){
 }
 var nextYear = getNextYear();
 console.log(nextYear);
+
+//Hãy tạo hàm getRandomItem, hàm này nhận 1 tham số là mảng và sẽ trả về ngẫu nhiên 1 phần tử của mảng.
+function getRandomItem(array) {
+    var random = Math.floor(Math.random() * array.length);
+    return array[random];
+  }
+  var myArray = ['a', 'b', 'c', 'd'];
+  var randomIndex = getRandomItem(myArray);
+  console.log(randomIndex);
+ 
+// Để vượt qua thử thách này, hãy hoàn thành hàm cho trước để đáp ứng các yêu cầu sau:
+//Khi a chia hết cho 3 thì return về 1
+//Khi a chia hết cho 5 thì return về 2
+//Khi a chia hết cho 15 thì return về 3
+  function run(a) {
+    if (a % 15 === 0) {
+      return 3;
+    } else if (a % 5 === 0) {
+      return 2;
+    } else if (a % 3 === 0) {
+      return 1;
+    } 
+  else {
+      return 0;
+    }
+  }
+console.log(run(3)) // 1
+console.log(run(5)) // 2
+console.log(run(15)) // 3
+console.log(run(13))// 0
+
+// Dùng switch case
+function run(fruits) {
+    var result;
+  switch (fruits) {
+  case "Banana":
+        result = "This is a Banana";
+        break;
+  case "Apple":
+        result = "This is an Apple";
+        break;
+  default: 
+        result = "Unknown fruit";
+    }
+  
+    return result;
+  }
+console.log(run("Banana")); // Output: This is a Banana
+console.log(run("Apple")); // Output: This is an Apple
+console.log(run("Orange")); // Output: Unknown fruit
+ 
+// toán tử 3 ngôi
+function getCanVoteMessage(age){
+    var result = age >= 18 ? 'Bạn có thể bỏ phiếu' : 'Bạn chưa được bỏ phiếu';
+    return result;
+}
+// Kỳ vọng
+console.log(getCanVoteMessage(18)) // 'Bạn có thể bỏ phiếu'
+console.log(getCanVoteMessage(15))// 'Bạn chưa được bỏ phiếu'
+
+function getRandNumbers(min, max, length) {
+    var result = [];
+    for (var i = 0; i < length; i++) {
+        var randomNumber = Math.random() * (max - min) + min;
+        result.push(randomNumber);
+    }
+    return result;
+}
+
+var randomNumbers = getRandNumbers(1, 10, 5);
+console.log(randomNumbers);
