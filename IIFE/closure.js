a70c29af232bdb4396d3e6d3187b93310718c49c
Ho Phuong <pho295157@gmail.com>
2023-09-06 17:06:21 +0700

    bind()method

--- IIFE/closure.js
+++ IIFE/closure.js
@@ -1,13 +1,68 @@
-function crateCounter() {
-    let counter = 0
 
-    function increase() {
-        return ++counter
-    }
+// function crateCounter() {
+//     let counter = 0
 
-    return increase
-}
+//     function increase() {
+//         return ++counter
+//     }
 
-const counter1 = crateCounter()
+//     return increase
+// }
 
-console.log(counter1())
+// const counter1 = crateCounter()
+
+// console.log(counter1())
+// let a = {
+//     name: 'Mercedes'
+// }
+// let b = a 
+
+//     a.name = 'BMW'
+
+// console.log(b)
+
+// const iPhone7 = {
+//     // Thuộc tính - Property
+//     name: 'iPhone 7',
+//     color: 'Pink',
+//     weight: 300,
+
+//     //Phương thức - Method
+//     takePhoto(){
+//         console.log('take a photo')
+//     }
+// }
+// console.log(iPhone7.takePhoto())
+
+// const button = document.querySelector('button')
+
+// button.onclick = function() {
+//     console.log(this.innerText)
+// }
+
+// this.firstName = 'Hồ'
+// this.lastName = 'PhƯơng'
+// this.age = 20
+// function getFullName() {
+//     console.log( `Họ: ${this.firstName}, Tên: ${this.lastName}, Tuổi: ${this.age}`)
+// }
+
+// const teacher = {
+//     firstName: 'Minh',
+//     lastName: 'Thảo',  
+// }
+// const student = {
+//     firstName: 'Nguyễn',
+//     lastName: 'Nhung',
+// }
+// const $ = getFullName.bind(student)
+// $(student.firstName = 'Hồng',student.age = 20)
+
+// const $$ = getFullName.bind(teacher)
+// $$(teacher.firstName = 'Pham',teacher.age = 22)
+
+// const button = document.querySelector('button')
+
+// button.onclick = getFullName.bind(student)
+
+   
