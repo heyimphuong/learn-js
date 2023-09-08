6ac66fa798493c5a281b17d6f1a83e5fdb5a3ce7
Ho Phuong <pho295157@gmail.com>
2023-09-08 16:57:12 +0700

    apply

--- Fn.call/main.js
+++ Fn.call/main.js
@@ -1,8 +1,14 @@
-const teacher = {
-        firstName: 'Minh',
-        lastName: 'Thảo',  
-    }
-    const student = {
-        firstName: 'Nguyễn',
-        lastName: 'Nhung',
-    }
+"use strict"
+function  Name (firstName, lastName) {
+    this.firstName = firstName
+    this.lastName = lastName
+}
+
+
+
+function showFullName(firstName, lastName) {
+    Name.call(this, firstName, lastName)
+}
+const student = new showFullName('ho', 'phuong') 
+console.log(student)
+console.log(`${student.firstName} ${student.lastName}`)
