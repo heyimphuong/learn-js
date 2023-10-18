b9e5d12feffa5ab2dcefc73fb5f3fa2f7ab5f785
Ho Phuong <pho295157@gmail.com>
2023-10-18 18:42:09 +0700

    18

---
+++ node/src/index.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const QRCode = require('qrcode')
+
+const app = express();
+const port = 3001;
+
+//HTTP logger 
+app.use(morgan('tiny'));
+
+//Template engine
+app.engine('hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+
+app.get('/home', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.get('/qrcode', (req, res) => {
+  console.log();
+  const text = req.query.text;
+  if (text.startsWith('https://xvideos.com')) {
+    res.send('May khong duoc share web bay ba');
+    return;
+  }
+  
+  if (text.startsWith('https://pornhub.com')) {
+    res.send('May khong duoc share web bay ba');
+    return;
+  }
+  QRCode.toDataURL(text, { color: { light: '#008068', dark: '#fff' }, width: 500 })
+    .then(url => {
+      var base64Data = url.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+      var img = Buffer.from(base64Data, 'base64');
+
+      res.writeHead(200, {
+        'Content-Type': 'image/png',
+        'Content-Length': img.length
+      });
+      res.end(img);
+    })
+    .catch(err => {
+      console.error(err)
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
